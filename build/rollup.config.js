a443797f6adfaec4006f3f1ae1d9f6d5586a6b31
lyohhhh <lyohhhh@163.com>
2022-06-15 15:56:57 +0800

    docs: Add rollup docs

--- build/rollup.config.js
+++ build/rollup.config.js
@@ -8,15 +8,20 @@ import commonjs from 'rollup-plugin-commonjs';
 import tailwindcss from 'tailwindcss';
 import { terser } from 'rollup-plugin-terser';
 
+// typescript 配置
 const overrides = {
-	compilerOptions: { declaration: true },
+	compilerOptions: { declaration: true }, // 是否生成 声明文件
 	exclude: ['node_modules', 'vite.config.ts'],
 };
+
+// 文件名称
 const file = type => `dist/${name}.${type}.js`;
 
 export { name, file };
 export default {
+	// 入口文件
 	input: 'src/index.ts',
+	// 输出文件
 	output: {
 		name,
 		file: file('esm'),
@@ -26,18 +31,33 @@ export default {
 		},
 	},
 	plugins: [
+		// 读取依赖文件 不适用该插件文件的引入路径是完整的
 		nodeResolve(),
+		// 打包 ts 文件 生成声明文件
 		typescript({ tsconfigOverride: overrides, lib: ['es5', 'es6', 'dom'], target: 'es5' }),
+		// css 文件
 		postcss({
+			// 转换的文件拓展名
 			extensions: ['.css', '.scss'],
+			// 输出的文件
 			extract: 'css/need-ui.css',
+			// 插件 css 引入
+			// tailwind
 			plugins: [postcssImport(), tailwindcss()],
 		}),
-		babel({ babelHelpers: 'bundled', extensions: ['.ts', '.js', '.tsx'] }),
+		// 使用 babel
+		babel({
+			babelHelpers: 'bundled',
+			// 转换的文件拓展名
+			extensions: ['.ts', '.js', '.tsx'],
+		}),
+		// 只压缩自己项目的代码
 		commonjs({
 			include: ['node_modules/**'],
 		}),
+		// 压缩代码
 		terser(),
 	],
+	// 不打包 vue 代码 从用户的依赖中获取
 	external: ['vue'],
 };
