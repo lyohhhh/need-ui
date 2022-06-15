92e2e834353dddad81cf4ed33b7a1f426f0fed01
lyohhhh <lyohhhh@163.com>
2022-06-15 14:30:08 +0800

    test: Test input compoent

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,12 @@
 <template>
-	<Button>测试</Button>
+	<l-input v-model="input"></l-input>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { LInput } from '../packages/input';
+import { ref } from 'vue';
+
+const input = ref('');
+</script>
 
 <style></style>
