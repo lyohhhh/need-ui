6e8c638316d97f6921a141ca31fd4553308687c6
lyohhhh <lyohhhh@163.com>
2022-06-16 09:56:41 +0800

    fix: Update resolve alias

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,6 @@
 import { defineConfig } from 'vite';
 import vue from '@vitejs/plugin-vue';
-// import { resolve } from 'path';
+import { resolve } from 'path';
 import vueJsx from '@vitejs/plugin-vue-jsx';
 
 // https://vitejs.dev/config/
@@ -10,8 +10,7 @@ export default defineConfig({
 	base: './',
 	resolve: {
 		alias: {
-			// '@': resolve('./packages'),
-			// '@shared': resolve('./packages/[shared]'),
+			'@': resolve('./packages'),
 		},
 		extensions: ['.js', '.tsx', '.vue', '.json', '.ts'],
 	},
