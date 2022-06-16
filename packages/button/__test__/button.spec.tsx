9492aeafbce66e17ee9e2f0cf185f42c1e4c5cb8
lyohhhh <lyohhhh@163.com>
2022-06-16 16:41:12 +0800

    test: Add button component test todo

--- packages/button/__test__/button.spec.tsx
+++ packages/button/__test__/button.spec.tsx
@@ -4,4 +4,10 @@ describe('button component', () => {
 	it('button mount', () => {
 		mount(LButton);
 	});
+
+	it.todo('button click', () => {});
+	it.todo('button loading', () => {});
+	it.todo('button disabled', () => {});
+	it.todo('button size', () => {});
+	it.todo('button type', () => {});
 });
