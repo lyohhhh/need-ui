168720d5a748f2b42f176f4cd9281cccd342b5a8
lyohhhh <lyohhhh@163.com>
2022-06-16 16:48:07 +0800

    test: Add form test

---
+++ packages/form/__test__/form.spec.tsx
@@ -0,0 +1,8 @@
+import { mount } from '@vue/test-utils';
+import { LForm } from '..';
+
+describe('form', () => {
+	it('form mount', () => {
+		mount(LForm);
+	});
+});
---
+++ packages/form/__test__/formItem.spec.tsx
@@ -0,0 +1,8 @@
+import { mount } from '@vue/test-utils';
+import { LFormItem } from '..';
+
+describe('form-item', () => {
+	it('form-item mount', () => {
+		mount(LFormItem);
+	});
+});
