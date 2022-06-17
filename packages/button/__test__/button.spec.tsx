978a709f1a4c794da9fa505ec74ae0286872dfb9
lyohhhh <lyohhhh@163.com>
2022-06-17 09:39:57 +0800

    test: add button click test

--- packages/button/__test__/button.spec.tsx
+++ packages/button/__test__/button.spec.tsx
@@ -1,11 +1,22 @@
 import { LButton } from '..';
 import { mount } from '@vue/test-utils';
+import { vi } from 'vitest';
 describe('button component', () => {
 	it('button mount', () => {
 		mount(LButton);
 	});
 
-	it.todo('button click', () => {});
+	it('button click', async () => {
+		const clickFn = vi.fn(() => 1);
+		const inst = mount(LButton, {
+			props: {
+				onClick: clickFn,
+			},
+		});
+		await inst.trigger('click');
+
+		expect(clickFn).toHaveBeenCalled();
+	});
 	it.todo('button loading', () => {});
 	it.todo('button disabled', () => {});
 	it.todo('button size', () => {});
