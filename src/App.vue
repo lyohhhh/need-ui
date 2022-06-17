3c6409d05dfb08502bf55136eb435b019c4bc531
lyohhhh <lyohhhh@163.com>
2022-06-17 10:11:26 +0800

    style: Update button type color

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,17 @@
 <template>
 	<LScroll>
-		<div style="height: 10000px"></div>
+		<div style="height: 10000px">
+			<LButton type="primary" size="xl">SUBMIT</LButton>
+			<LButton type="default" size="lg">RESET</LButton>
+			<LButton type="danger" size="base">DANGER</LButton>
+			<LButton type="warning" size="sm">WARNING</LButton>
+			<LButton type="success" size="xs">SUCCESS</LButton>
+		</div>
 	</LScroll>
 </template>
 
 <script setup lang="ts">
-import { LInput, LScroll } from '../packages/components';
+import { LButton, LScroll } from '../packages/components';
 import { ref } from 'vue';
 
 const input = ref('');
