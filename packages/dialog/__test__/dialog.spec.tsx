6af9a5b0b38d27dc42247730206aa6b3664d951a
lyohhhh <lyohhhh@163.com>
2022-06-21 10:40:34 +0800

    test: add dialog todo test

--- packages/dialog/__test__/dialog.spec.tsx
+++ packages/dialog/__test__/dialog.spec.tsx
@@ -1,8 +1,22 @@
 import { mount } from '@vue/test-utils';
+import { nextTick } from 'vue';
 import { LDialog } from '..';
 
 describe('dialog', () => {
 	it('dialog mount', () => {
 		mount(LDialog);
+	});
+
+	it.todo('dialog fn', async () => {
+		const cancelFn = vi.fn(() => 'cancel');
+		const confirmFn = vi.fn(() => 'confirm');
+		const inst = mount(LDialog, {
+			props: {
+				onCancel: cancelFn,
+				onConfirm: confirmFn,
+				modelValue: true,
+			},
+		});
+		await nextTick();
 	});
 });
