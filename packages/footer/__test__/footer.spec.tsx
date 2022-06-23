b9d8aaf1c70123e23a6260a58737d3bfc3afe60b
lyohhhh <lyohhhh@163.com>
2022-06-23 10:34:29 +0800

    test: add footer test slots default

--- packages/footer/__test__/footer.spec.tsx
+++ packages/footer/__test__/footer.spec.tsx
@@ -5,4 +5,14 @@ describe('footer', () => {
 	it('footer mount', () => {
 		mount(LFooter);
 	});
+
+	it('footer slots `default`', () => {
+		const inst = mount(LFooter, {
+			slots: {
+				default: '测试',
+			},
+		});
+
+		expect(inst.find('.footer').text()).toEqual('测试');
+	});
 });
--- packages/footer/src/footer.tsx
+++ packages/footer/src/footer.tsx
@@ -1,11 +1,13 @@
-import { defineComponent } from 'vue';
+import { defineComponent, renderSlot } from 'vue';
 export default defineComponent({
 	name: 'Footer',
 
 	render() {
 		return (
 			<>
-				<footer class='fixed shadow-top-md left-0 right-0 bottom-0 h-16 bg-white dark:shadow-themebgcolor-700 dark:bg-themebgcolor-900'></footer>
+				<footer class='footer fixed shadow-top-md left-0 right-0 bottom-0 h-16 bg-white dark:shadow-themebgcolor-700 dark:bg-themebgcolor-900'>
+					{renderSlot(this.$slots, 'default')}
+				</footer>
 			</>
 		);
 	},
