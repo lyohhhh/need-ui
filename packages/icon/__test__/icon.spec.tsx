54d6e51d8b7e51e2124f406413b7081d1a591ee0
lyohhhh <lyohhhh@163.com>
2022-06-24 14:52:09 +0800

    test: add icon click test

--- packages/icon/__test__/icon.spec.tsx
+++ packages/icon/__test__/icon.spec.tsx
@@ -1,8 +1,22 @@
 import { mount } from '@vue/test-utils';
 import { LIcon } from '..';
+import { vi } from 'vitest';
 
 describe('icon', () => {
 	it('icon mount', () => {
 		mount(LIcon);
+	});
+
+	it('icon click ', () => {
+		const clickFunc = vi.fn(() => 1);
+		const inst = mount(LIcon, {
+			props: {
+				icon: 'dark',
+				onClick: clickFunc,
+			},
+		});
+
+		inst.trigger('click');
+		expect(clickFunc).toHaveBeenCalled();
 	});
 });
