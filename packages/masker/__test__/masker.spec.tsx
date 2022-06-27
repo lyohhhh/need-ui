70e9cbc6947512cdc1af80456c9596be3ce965ef
lyohhhh <lyohhhh@163.com>
2022-06-27 16:06:45 +0800

    test: masker close test

--- packages/masker/__test__/masker.spec.tsx
+++ packages/masker/__test__/masker.spec.tsx
@@ -1,8 +1,26 @@
 import { mount } from '@vue/test-utils';
+import { nextTick, ref } from 'vue';
 import { LMasker } from '..';
+import { vi } from 'vitest';
 
 describe('masker', () => {
-	it.only('masker mount', () => {
+	it('masker mount', () => {
 		mount(LMasker);
+	});
+
+	it.todo('masker close', async () => {
+		const show = ref<boolean>(true);
+		const changeHandle = vi.fn(() => {
+			show.value = false;
+		});
+		const wrapper = mount(LMasker, {
+			props: {
+				show: show.value,
+				onChange: changeHandle,
+			},
+		});
+		await nextTick();
+		await wrapper.trigger('change');
+		expect(changeHandle).toBeCalled();
 	});
 });
