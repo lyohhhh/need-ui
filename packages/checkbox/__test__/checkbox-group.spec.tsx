381fa376d6e4fec9e211d18593d246d60e6be30c
lyohhhh <lyohhhh@163.com>
2022-07-10 20:38:27 +0800

    test: add checkbox group disabled test

--- packages/checkbox/__test__/checkbox-group.spec.tsx
+++ packages/checkbox/__test__/checkbox-group.spec.tsx
@@ -1,15 +1,23 @@
 import { mount } from '@vue/test-utils';
-import { LCheckboxGroup } from '..';
+import { LCheckboxGroup, LCheckbox } from '..';
 
 describe('checkbox-group', () => {
 	it('checkbox-group mount', () => {
 		mount(LCheckboxGroup);
 	});
 
-	
-	it.todo(`checkbox 'disabled' props` ,() => {
-		mount(LCheckboxGroup)
-	})
-	it.todo(`checkbox 'border' props` ,() => {})
-	it.todo(`checkbox 'change' func` ,() => {})
+	it.only(`checkbox-group 'disabled' props`, () => {
+		const wrapper = mount(LCheckboxGroup, {
+			slots: {
+				default: <LCheckbox label={1}></LCheckbox>,
+			},
+			props: {
+				disabled: true,
+			},
+		});
+
+		expect(wrapper.find('.is-disabled').exists()).toBe(true);
+	});
+	it.todo(`checkbox group 'border' props`, () => {});
+	it.todo(`checkbox group 'change' func`, () => {});
 });
--- packages/checkbox/src/checkbox.tsx
+++ packages/checkbox/src/checkbox.tsx
@@ -51,7 +51,7 @@ export default defineComponent({
 		});
 
 		const isCheckedByGroup = (): boolean => {
-			return !!(injectValue && injectValue.value.includes(props.label || ''));
+			return !!(injectValue && injectValue?.value?.includes(props.label || ''));
 		};
 
 		const isShowIcon = computed<boolean>(() => {
