6cc6e320dd35ebbb615932e10a6179b41d78f9fa
lyohhhh <lyohhhh@163.com>
2022-07-11 14:25:16 +0800

    test: add checkbox group change func props

--- packages/checkbox/__test__/checkbox-group.spec.tsx
+++ packages/checkbox/__test__/checkbox-group.spec.tsx
@@ -1,5 +1,6 @@
 import { mount } from '@vue/test-utils';
 import { vi } from 'vitest';
+import { ref } from 'vue';
 import { LCheckboxGroup, LCheckbox } from '..';
 
 describe('checkbox-group', () => {
@@ -38,5 +39,34 @@ describe('checkbox-group', () => {
 		expect(wrapper.find('.is-border').exists()).toBe(true);
 	});
 
-	it.todo(`checkbox group 'change' func`, () => {});
+	it.todo(`checkbox group 'change' func`, async () => {
+		const changeFn = vi.fn();
+		const checkedValue = ref<number>(0);
+		const wrapper = mount(LCheckboxGroup, {
+			slots: {
+				default: (
+					<>
+						<LCheckbox label={1}></LCheckbox>
+						<LCheckbox label={2}></LCheckbox>
+						<LCheckbox label={3}></LCheckbox>
+					</>
+				),
+			},
+			props: {
+				disabled: true,
+				onChange: changeFn,
+				modelValue: checkedValue as any,
+			},
+		});
+
+		expect(changeFn).not.toBeCalled();
+		await wrapper.findAllComponents(LCheckbox)[2].find('.l-radio').trigger('click');
+
+		expect(changeFn).toBeCalled();
+		expect(checkedValue.value).toContain(2);
+
+		await wrapper.findAllComponents(LCheckbox)[1].find('.l-radio').trigger('click');
+		expect(changeFn).toBeCalledTimes(2);
+		expect(checkedValue.value).toContain(1);
+	});
 });
