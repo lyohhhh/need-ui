fd5f94ff2fa7a840eedbfd676f965b14ccfdeb44
lyohhhh <lyohhhh@163.com>
2022-07-16 22:31:24 +0800

    test: add input number disabled test

--- packages/input-number/__test__/input=number.spec.tsx
+++ packages/input-number/__test__/input=number.spec.tsx
@@ -1,10 +1,19 @@
 import { mount } from '@vue/test-utils';
 import { LInputNumber } from '..';
+import { vi } from 'vitest';
 
 describe('input-number', () => {
 	it('mount input-number component', () => {
 		mount(LInputNumber);
 	});
 
-	it.todo(`input-number 'disabled' props`, () => {});
+	it.todo(`input-number 'disabled' props`, () => {
+		const changeFn = vi.fn();
+		const wrapper = mount(LInputNumber, {
+			props: {
+				disabled: true,
+				onChange: changeFn,
+			},
+		});
+	});
 });
