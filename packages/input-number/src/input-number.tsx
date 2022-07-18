3554daba90a3370e693f362f095f32aca153870f
lyohhhh <lyohhhh@163.com>
2022-07-18 17:45:26 +0800

    fix: add input number emit func

--- packages/input-number/src/input-number.tsx
+++ packages/input-number/src/input-number.tsx
@@ -6,11 +6,27 @@ export default defineComponent({
 	name: 'InputNumber',
 	props: InputNumberProps,
 	emits: ['update:modelValue', 'focus', 'blur', 'change'],
-	setup() {
+	setup(_props, { emit }) {
+		const emitChange = (args: number) => {
+			emit('change', args);
+		};
+		const emitFocus = (args: number) => {
+			emit('focus', args);
+		};
+
+		const emitBlur = (args: number) => {
+			emit('focus', args);
+		};
+
 		return () => (
 			<>
 				<div class='l-input-number'>
-					<LInput type='number'></LInput>
+					<LInput
+						type='number'
+						onChange={emitChange}
+						onFocus={emitFocus}
+						onBlur={emitBlur}
+					></LInput>
 				</div>
 			</>
 		);
