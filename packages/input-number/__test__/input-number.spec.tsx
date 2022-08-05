a35776188cace4dd50e830747772a7f025c5ef2a
lyohhhh <lyohhhh@163.com>
2022-08-05 14:51:39 +0800

    test: add input number test

--- packages/input-number/__test__/input-number.spec.tsx
+++ packages/input-number/__test__/input-number.spec.tsx
@@ -1,6 +1,7 @@
 import { mount } from '@vue/test-utils';
 import { LInputNumber } from '..';
 import { vi } from 'vitest';
+import { ref } from 'vue';
 
 describe('input-number', () => {
 	it('mount input-number component', () => {
@@ -20,8 +21,17 @@ describe('input-number', () => {
 		expect(changeFn).not.toBeCalled();
 	});
 
-	it.todo(`input-number 'add' func`, () => {
-		const wrapper = mount(LInputNumber);
+	it.todo(`input-number 'add' func`, async () => {
+		const model = ref<number>(0);
+		const changeFn = vi.fn();
+		const wrapper = mount(LInputNumber, {
+			props: {
+				modelValue: model as any,
+				onChange: changeFn,
+			},
+		});
+		expect(changeFn).not.toBeCalled();
+		await wrapper.find('.l-input-number__suffix .l-icon').trigger('click');
 	});
 	it.todo(`input-number 'minus' func`, () => {
 		const wrapper = mount(LInputNumber);
