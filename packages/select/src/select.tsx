a8dbaa8b17668d69be674de98fe022dcabc64e9d
lyohhhh <lyohhhh@163.com>
2022-08-08 15:39:55 +0800

    chore: add select props

--- packages/select/src/select.tsx
+++ packages/select/src/select.tsx
@@ -1,7 +1,9 @@
 import { defineComponent } from 'vue';
+import { SelectProps } from './select.props';
 
 export default defineComponent({
 	name: 'Select',
+	props: SelectProps,
 	setup(_props) {},
 	render() {
 		return <div>select</div>;
