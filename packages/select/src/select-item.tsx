9e65129d5ad7c21ef1afab38e7ca0554aabe1c16
lyohhhh <lyohhhh@163.com>
2022-08-24 23:21:03 +0800

    style: add select item class computed

--- packages/select/src/select-item.tsx
+++ packages/select/src/select-item.tsx
@@ -1,10 +1,19 @@
-import { defineComponent } from 'vue';
+import { computed, defineComponent } from 'vue';
 import { SelectItemProps } from './select-item-props';
 
 export default defineComponent({
 	name: 'SelectItem',
 	props: SelectItemProps,
-	setup(_props) {},
+	setup(_props) {
+		const classes = computed<string>(() => {
+			let classStr: string[] = [];
+			return classStr.join(' ');
+		});
+
+		return {
+			classes,
+		};
+	},
 	render() {
 		return <div class='l-select-item'></div>;
 	},
