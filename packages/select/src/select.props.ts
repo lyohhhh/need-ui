e8f263b544732f25589b0baff39738702d41d338
lyohhhh <lyohhhh@163.com>
2022-08-27 23:03:26 +0800

    chore: add select props

--- packages/select/src/select.props.ts
+++ packages/select/src/select.props.ts
@@ -1,1 +1,49 @@
-export const SelectProps = {};
+export const SelectProps = {
+	modelValue: {
+		type: [String, Number],
+		required: true,
+	},
+	multiple: {
+		type: Boolean,
+		default: false,
+	},
+	disabled: {
+		type: Boolean,
+		default: false,
+	},
+	clearable: {
+		type: Boolean,
+		default: false,
+	},
+	name: String,
+	placeholder: {
+		type: String,
+		default: '请选择',
+	},
+	filterable: {
+		type: Boolean,
+		default: false,
+	},
+	filterMethod: Function,
+	remote: {
+		type: Boolean,
+		default: false,
+	},
+	remoteFunction: Function,
+	loading: {
+		type: Boolean,
+		default: false,
+	},
+	loadingText: {
+		type: String,
+		default: '加载中',
+	},
+	noMatchText: {
+		type: String,
+		default: '暂无匹配数据',
+	},
+	noDataText: {
+		type: String,
+		default: '暂无数据',
+	},
+} as const;
